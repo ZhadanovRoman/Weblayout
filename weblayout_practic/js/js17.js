46066e066ec57435ba6ce88d7caa78a7dc3c54fc
Роман <48046924+ZhadanovRoman@users.noreply.github.com>
2023-06-02 09:35:05 +0300

    Add files via upload

---
+++ weblayout_practic/js/js17.js
@@ -0,0 +1,21 @@
+
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header-nav2');
+let menuLinks = document.querySelectorAll('.header-link');
+
+burger.addEventListener('click',
+function(){
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('header-nav--active');
+  document.body.classList.toggle('.stop-scroll');
+})
+
+menuLinks.forEach( function(el){
+  el.addEventListener('click',function(){
+    burger.classList.remove('burger--active');
+    menu.classList.remove('header-nav--active');
+    document.body.classList.remove('.stop-scroll');
+
+
+  })
+})
