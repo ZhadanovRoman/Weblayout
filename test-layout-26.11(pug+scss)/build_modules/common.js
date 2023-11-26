fa5f31a347e9ee39aa045c61a9f724dc6c1ca8e9
Roman <romanoff198928@gmail.com>
2023-11-26 21:47:25 +0300

    Revert "test-work"
    This reverts commit a75f3d98c309013607a489485c0aee6b12938b9f.

--- test-layout-26.11(pug+scss)/build_modules/common.js
+++
@@ -1,6 +0,0 @@
-import './common.scss';
-import '../src/app/js/common';
-
-// SVG
-const requireAll = (r) => r.keys().forEach(r);
-requireAll(require.context('../src/assets/icons', true, /\.svg$/));
