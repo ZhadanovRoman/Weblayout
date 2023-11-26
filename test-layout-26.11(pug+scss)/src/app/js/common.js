a75f3d98c309013607a489485c0aee6b12938b9f
Roman <romanoff198928@gmail.com>
2023-11-26 21:43:24 +0300

    test-work

---
+++ test-layout-26.11(pug+scss)/src/app/js/common.js
@@ -0,0 +1,35 @@
+import Header from "../../components/header/header";
+import Main from "../../components/main/main";
+import Footer from "../../components/footer/footer";
+import Contacts from "../../components/contacts/contacts";
+/**
+ * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
+ * Ключ объекта - Название компонента, которое было указано во время его создания (совпадает с именем файлов)
+ * Значение - JS-класс компонента (Импорт добавляется вручную)
+ */
+const allComponents = {
+    'header': Header,
+    'main': Main,
+    'footer': Footer,
+    'contacts': Contacts
+}
+
+/**
+ * Инициализация всех компонентов на странице
+ */
+try {
+    const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
+
+    const components = existedComponents.map((component) => {
+        try {
+            return new allComponents[component.dataset.component]({
+                name: component.dataset.component,
+                component: component,
+            });
+        } catch (e) {
+            console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
+        }
+    });
+} catch (e) {
+    console.error(e);
+}
