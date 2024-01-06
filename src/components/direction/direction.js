429970e979f64a2f0e97770215b70a4d176987c1
Roman <romanoff198928@gmail.com>
2024-01-06 14:28:38 +0300

    adaptive+

--- src/components/direction/direction.js
+++ src/components/direction/direction.js
@@ -1,13 +1,50 @@
 import Component from '../../app/js/base/component';
 
 class Direction extends Component {
+    arrow;
+    item;
+    paragraph;
+    marker;
+    dubleMarker;
     constructor(element) {
         super(element);
 
+        this.arrow = this.getElements('accordArrow');
+        this.item = this.getElements('item');
+        this.paragraph = this.getElements('dscr')
+        this.marker = false;
+        this.dubleMarker = true;
+
+        for (let i = 0; i < this.arrow.length; i++) {
+
+            this.item[i].addEventListener('click', () => {
+               this.item.forEach((el)=>{
+                   if(el.childNodes[1].classList.contains('paragraph-mrgn') && this.dubleMarker===false){
+                        this.dubleMarker=true;
+                        el.childNodes[0].childNodes[1].classList.remove('arrowRotate')
+                        el.childNodes[1].style.maxHeight = null;
+                        el.childNodes[1].classList.remove('paragraph-mrgn');
+                  
+                    }
+                })
+                if (this.marker === false ) {
+                    this.arrow[i].classList.add('arrowRotate')
+                    this.paragraph[i].style.maxHeight = this.paragraph[i].scrollHeight + 'px';
+                    this.paragraph[i].classList.add('paragraph-mrgn');
+                    this.marker=true;
+                    this.dubleMarker=false;
+                }else{
+                    this.arrow[i].classList.remove('arrowRotate')
+                    this.paragraph[i].style.maxHeight = null;
+                    this.paragraph[i].classList.remove('paragraph-mrgn');
+                    this.marker=false;
+                }
+            })
+        }
         // Your code here
     }
 
-    // Your code here
+
 }
 
 export default Direction
