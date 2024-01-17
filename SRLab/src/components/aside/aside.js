1865cb35772ee45d684350516aa45f3619904c73
Roman <romanoff198928@gmail.com>
2024-01-17 21:44:49 +0300

    refact17.01

--- SRLab/src/components/aside/aside.js
+++ SRLab/src/components/aside/aside.js
@@ -3,22 +3,39 @@ import Component from '../../app/js/base/component';
 
 class Aside extends Component {
     sizeBlock;
+    btnReset;
+    checkBox;
     constructor(element) {
         super(element);
-        
-            this.sizeBlock = this.getElements('size-check')
-        
-        
+        this.checkBox = document.querySelectorAll('.check-box__inpt');
+        this.btnReset = this.getElement('reset-btn');
 
-     this.sizeBlock.forEach((el)=>{
+        this.sizeBlock = this.getElements('size-check');
+        this.btnReset.addEventListener('click', this.btnResetClick)
+
+
+        this.sizeBlock.forEach((el) => {
             el.addEventListener('click', this.sizeBlockClick)
         })
     }
 
-    sizeBlockClick =(e)=>{
-        e.target.classList.toggle('size-block-change')      
+    sizeBlockClick = (e) => {
+        e.target.classList.toggle('size-block-change')
     }
-    
+    btnResetClick = () => {
+        this.checkBox.forEach((el)=>{
+            if(el.checked){
+                el.checked = false;
+            }
+        })
+
+        this.sizeBlock.forEach((el) => {
+            if(el.classList.contains('size-block-change')){
+                el.classList.remove('size-block-change')
+            }
+        })
+    }
+
 }
 
 export default Aside
--- SRLab/src/components/check-box/check-box.js
+++ SRLab/src/components/check-box/check-box.js
@@ -1,10 +1,10 @@
 import Component from '../../app/js/base/component';
 
 class CheckBox extends Component {
+    
     constructor(element) {
         super(element);
-
-        // Your code here
+        
     }
 
     // Your code here
