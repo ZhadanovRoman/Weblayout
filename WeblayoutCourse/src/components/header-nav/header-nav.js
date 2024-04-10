5a5000e03a9dbbf70ca0bc3aaaf308efe5afffb3
Roman <romanoff198928@gmail.com>
2024-04-10 10:28:32 +0300

    CourseWork

---
+++ WeblayoutCourse/src/components/header-nav/header-nav.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class HeaderNav extends Component {
+    constructor(element) {
+        super(element);
+
+     
+    }
+
+   
+}
+
+export default HeaderNav
---
+++ WeblayoutCourse/src/components/main/main.js
@@ -0,0 +1,15 @@
+import Component from '../../app/js/base/component';
+
+class Main extends Component {
+
+    constructor(element) {
+        super(element);
+
+
+    }
+
+
+
+}
+
+export default Main
